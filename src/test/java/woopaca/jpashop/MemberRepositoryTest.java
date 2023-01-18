bffea255c850f26ac1e03e3af0aa05c09d200294
woopaca <jcw001031@gmail.com>
2023-01-18 16:53:32 +0900

    DB 접근 테스트

---
+++ src/test/java/woopaca/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package woopaca.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void 회원_저장() throws Exception {
+        //given
+        Member member = new Member();
+        System.out.println("member = " + member);
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isSameAs(member);
+    }
+}
