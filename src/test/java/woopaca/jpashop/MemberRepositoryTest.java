42ec89bedcf3a7449e11257ac3b23cac4305f6c9
woopaca <jcw001031@gmail.com>
2023-01-18 18:47:25 +0900

    SQL logger 외부 라이브러리 적용

--- src/test/java/woopaca/jpashop/MemberRepositoryTest.java
+++ src/test/java/woopaca/jpashop/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package woopaca.jpashop;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,10 +16,11 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(value = false)
     void 회원_저장() throws Exception {
         //given
         Member member = new Member();
-        System.out.println("member = " + member);
+        member.setUsername("memberA");
 
         //when
         Long savedId = memberRepository.save(member);
