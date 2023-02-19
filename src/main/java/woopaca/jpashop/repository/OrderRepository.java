b914da8e0247659dcb583a2f76cb1f996896864a
지찬우 <jcw001031@gmail.com>
2023-02-19 14:56:40 +0900

    주문 서비스 개발

---
+++ src/main/java/woopaca/jpashop/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package woopaca.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import woopaca.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll() {
+//        return null;
+//    }
+}
